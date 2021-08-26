656a2bb005f2c6f9f20e21d7573e74b1c60876bd
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2021-08-26 18:25:09 +0530

    Fixed major body css bug and minor color changes

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,13 +3,11 @@ const theme = document.querySelector("#theme-link");
 
 btn.addEventListener("click", function () {
   if (theme.getAttribute("href") != "css/darkreader.css") {
-    console.log("It's currently light mode, changing to dark mode now");
     theme.href = "css/darkreader.css";
     document.querySelector(".chakra-icon").src = "/images/dark.svg";
     btn.classList.remove("css-1h84u6v");
     btn.classList.add("css-xpotm3");
   } else {
-    console.log("It's currently dark mode, changing to light mode now");
     theme.href = "css/empty.css";
     document.querySelector(".chakra-icon").src = "/images/light.svg";
     btn.classList.remove("css-xpotm3");
